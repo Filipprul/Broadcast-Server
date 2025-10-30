dac61097a4a1c8491946968619856c4bde9c6513
Filipp <filipprul2007@gmail.com>
2025-10-30 23:39:05 +0100

    Update POM to use Java 17; modify BroadcastServer to bind to all interfaces and add shutdown method; add initial HTML UI for Broadcast Chat

--- src/main/java/BroadcastServer.java
+++ src/main/java/BroadcastServer.java
@@ -2,6 +2,7 @@ import org.java_websocket.server.WebSocketServer;
 import org.java_websocket.WebSocket;
 import org.java_websocket.handshake.ClientHandshake;
 
+import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.util.Collections;
 import java.util.HashSet;
@@ -12,7 +13,7 @@ public class BroadcastServer extends WebSocketServer {
     private final Set<WebSocket> clients = Collections.synchronizedSet(new HashSet<>());
 
     public BroadcastServer(int port) {
-        super(new InetSocketAddress(port));
+        super(new InetSocketAddress("0.0.0.0", port));
     }
 
     @Override
@@ -49,6 +50,16 @@ public class BroadcastServer extends WebSocketServer {
             for (WebSocket client : clients) {
                 client.send(message);
             }
+        }
+    }
+
+    @SuppressWarnings("CallToPrintStackTrace")
+    public void shutdown(){
+        try {
+            this.stop();
+            System.out.println("Server wurde sauber gestoppt.");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 }
